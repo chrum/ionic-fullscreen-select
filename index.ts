4c3282a2900182fd3d1d7195eba3cfad6eb94c63
chrystian <chrystianr@gmail.com>
2017-11-27 11:20:12 +0100

    ng v5 adjustments

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import {NgModule, ModuleWithProviders, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
-import {CommonModule} from "@angular/common";
-import {IonicModule} from "ionic-angular";
+import {CommonModule} from '@angular/common';
+import {IonicModule} from 'ionic-angular';
 
 import {FullScreenSelectComponent} from './src/ion-fullscreen-select';
 import {FullScreenSelectPage} from './src/ion-fullscreen-select-page';
@@ -9,6 +9,7 @@ import {FullScreenSelectPage} from './src/ion-fullscreen-select-page';
 @NgModule({
     declarations: [
         FullScreenSelectPage,
+        FullScreenSelectComponent
     ],
     entryComponents: [
         FullScreenSelectPage
